4634970dfd8b6ae45c3789814839d7392cef8944
Baraasher <Baraa.programmer@gmail.com>
2023-03-05 00:22:04 +0200

    update layouts

--- scripts/script.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("aboutMe").addEventListener("click",function(){document.getElementById("modal").style.display="flex"}),document.getElementById("closeModal").addEventListener("click",function(){document.getElementById("modal").style.display="none"});
