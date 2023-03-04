dae0d3d8636912dea780eff2ee592df47a72e44b
Baraasher <Baraa.programmer@gmail.com>
2023-03-04 23:25:40 +0200

    update layouts

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,1 @@
-document.getElementById("aboutMe").addEventListener('click' , function(){
-
-
-document.getElementById("modal").style.display = "flex"
-
-});
-
-
-
-document.getElementById("closeModal").addEventListener('click' , function(){
-
-
-    document.getElementById("modal").style.display = "none"
-    
-    });
-
+document.getElementById("aboutMe").addEventListener("click",function(){document.getElementById("modal").style.display="flex"}),document.getElementById("closeModal").addEventListener("click",function(){document.getElementById("modal").style.display="none"});
